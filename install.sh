ed47f3510aacdfadfafc9d6dfdd1ca61dd8dcf42
klynger <rafael.klynger@gmail.com>
2025-01-05 19:33:11 -0300

    Create symlinks script

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 . scripts/utils.sh
 . scripts/prerequisites.sh
+. scripts/symlinks.sh
 
 info "Dotfiles installation initialized…"
 read -p "Install apps? [Y/n] " install_apps
+read -p "Overwrite existing dotfiles? [y/n] " overwrite_dotfiles
 
 install_apps=${install_apps:-y}
 
@@ -13,7 +15,26 @@ if [[ "$install_apps" == "y" ]]; then
     info "====================="
     info "Apps"
     info "====================="
+    printf "\n"
 
     install_homebrew
     install_xcode
+else
+    warning "Apps won't be installed"
 fi
+
+printf "\n"
+printf "\n"
+info "====================="
+info "Symbolic links"
+info "====================="
+printf "\n"
+
+chmod +x ./scripts/symlinks.sh
+if [[ "$overwrite_dotfiles" == "y" ]]; then
+    warning "Deleting existing dotfiles..."
+    ./scripts/symlinks.sh --delete --include-files
+fi
+./scripts/symlinks.sh --create
+
+success "Dotfiles set up successfully."
