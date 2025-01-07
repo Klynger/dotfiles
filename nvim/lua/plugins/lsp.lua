016a9574d2fe6a27db322df1732e3e7e2bc1f540
klynger <rafael.klynger@gmail.com>
2025-01-07 19:20:06 -0300

    Add missing possible tailwind config to lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -150,6 +150,7 @@ return {
         filetypes = { 'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'css' },
         root_dir = require('lspconfig').util.root_pattern(
           'tailwind.config.js',
+          'tailwind.config.cjs',
           'tailwind.config.ts',
           'postcss.config.js',
           'node_modules'
